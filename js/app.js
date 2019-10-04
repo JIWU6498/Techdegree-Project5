07d07a7a2ebb7c54b255590289a7057fb5205b9e
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-04 18:25:17 +1000

    add hideseek jquery plugin

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
+//jQuery Plugin for sticky navigation bar
 $("#header").sticky({
    topSpacing: 0
 });
 
+//jQuery Plugin for lightBox
 $(".venobox").venobox({
    framewidth: '800px',        
-   titleattr: 'title',    // default: 'title'
-   numeratio: true,            // default: false
-   infinigall: true ,           // default: false
+   titleattr: 'title',    
+   numeratio: true,            
+   infinigall: true ,          
    titlePosition: "bottom",
    numeratio:true
 });
+
+//jQuery Plugin for keywords filtering
+$(".searchInput").hideseek({
+   attribute: 'data-name',
+});
+
+//javascript for searchbox filtering
+// $(".searchInput").on("keyup", function() {
+//    const value = $(this).val().toLowerCase();
+//    $("a[title]").filter(function() {
+//      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//    });
+//  });
+
