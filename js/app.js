750d0b3d5742281e079f51e138f7f6d4049466f2
Jianing Wu <jiwu6498@uni.sydney.edu.au>
2019-10-04 15:30:29 +1000

    add venobox lightbox jquery plugin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 $("#header").sticky({
    topSpacing: 0
 });
+
+$(".venobox").venobox({
+   framewidth: '800px',        
+   titleattr: 'title',    // default: 'title'
+   numeratio: true,            // default: false
+   infinigall: true ,           // default: false
+   titlePosition: "bottom",
+   numeratio:true
+});
